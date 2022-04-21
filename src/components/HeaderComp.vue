ac5ef3a6d92c89754e975988fc8408d7655d7ef5
Ga <gabri.magni.97@gmail.com>
2022-04-21 14:42:18 +0200

    select, (disabled selected)

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="fs-2 w-100 p-3">
-    <img src="../assets/img/logo.svg" alt="">
+  <div class="p-3 d-flex justify-content-between align-items-center">
+    <div class="">
+      <img src="../assets/img/logo.svg" alt="">
+    </div>
+
+    <div>
+      <select name="select_genre" id="select_genre" class="p-2">
+        <option value="" disabled selected>Scegli un genere</option>
+        <option value="Rock">Rock</option>
+        <option value="Pop">Pop</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Metal">Metal</option>
+      </select>
+    </div>
   </div>
 </template>
 
